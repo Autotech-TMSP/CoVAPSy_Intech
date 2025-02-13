5593e92963d502d506f17c85b5912479476efed7
Danura30082 <135959478+Danura30082@users.noreply.github.com>
2025-02-13 23:38:12 +0100

    fix: correct direction_prop to enable reverse operation for motor controllers

--- src/HL/main.py
+++ src/HL/main.py
@@ -19,7 +19,7 @@ class Car():
         self.PORT = 10940
 
         # Paramètres de la fonction vitesse_m_s
-        self.direction_prop = 1                                                             # -1 pour les variateurs inversés ou un petit rapport correspond à une marche avant
+        self.direction_prop = -1                                                             # -1 pour les variateurs inversés ou un petit rapport correspond à une marche avant
         self.pwm_stop_prop = 7.53
         self.point_mort_prop = 0.5
         self.delta_pwm_max_prop = 1.1                                                       # pwm à laquelle on atteint la vitesse maximale
